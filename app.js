775068bf953eaf87e1fb6795a34f706fb17e6ba5
Glenn Stovall <glennstovall@gmail.com>
2013-02-04 22:12:01 -0500

    syntax error fix

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ var io = require('socket.io').listen(server);
 
 //express.js configuration options
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
   //app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.static(path.join(process.env.PWD,'public'));
+    app.use(express.static(path.join(process.env.PWD,'public')));
 });
 
 app.configure('development', function(){var fs = require('fs');
